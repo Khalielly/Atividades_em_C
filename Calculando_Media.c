fa587ae4db4d835e047b1d153c607936a3f7e970
Danielly Khalil Cândido Teixeira <152915978+Khalielly@users.noreply.github.com>
2023-12-04 16:51:00 -0300

    Add files via upload

---
+++ Calculando_Media.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+
+int main(){
+    float n1, n2, n3, n4, media;
+
+    scanf("%f %f %f %f", &n1, &n2, &n3, &n4);
+
+    media = (n1 + n2 + n3 + n4)/4;
+
+    if(media >= 7){
+        printf(" Média = %.2f - Aluno Aprovado", media);
+    }
+
+    else if (media < 7){
+        printf(" Media = %.2f - Aluno Reprovado", media);
+    }
+
+    return 0;
+}

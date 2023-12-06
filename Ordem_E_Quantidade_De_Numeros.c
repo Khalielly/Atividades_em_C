8af51ae7b422ca57e7a775869cd708981ee909be
Danielly Khalil Cândido Teixeira <152915978+Khalielly@users.noreply.github.com>
2023-12-06 12:46:05 -0300

    Add files via upload

---
+++ Ordem_E_Quantidade_De_Numeros.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+void Ordem_dos_numeros(int vetor[], int n);
+
+int main() {
+    int vetor[100];
+    int n;
+
+    printf("Digite a quantidade de numeros: ");
+    scanf("%d", &n);
+
+    printf("Digite os numeros: ");
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &vetor[i]);
+    }
+
+    Ordem_dos_numeros(vetor, n);
+
+    printf("Numeros ordenados: ");
+    for (int i = 0; i < n; i++) {
+        printf("%d ", vetor[i]);
+    }
+
+    return 0;
+}
+
+void Ordem_dos_numeros(int vetor[], int n) {
+    int troca;
+
+    for (int i = 0; i < n; i++) {
+        for (int j = i + 1; j < n; j++) {
+            if (vetor[i] > vetor[j]) {
+                troca = vetor[i];
+                vetor[i] = vetor[j];
+                vetor[j] = troca;
+            }
+        }
+    }
+}

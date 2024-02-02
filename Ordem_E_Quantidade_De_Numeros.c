dfa94bbf5ef7ab089d38c4df40d4dc1da503b765
Danielly Khalil Cândido Teixeira <152915978+Khalielly@users.noreply.github.com>
2024-02-02 19:20:00 -0300

    Update Ordem_E_Quantidade_De_Numeros.c

--- Ordem_E_Quantidade_De_Numeros.c
+++ Ordem_E_Quantidade_De_Numeros.c
@@ -1,31 +1,31 @@
 #include <stdio.h>
 
-void Ordem_dos_numeros(int vetor[], int n);
+void Ordem_dos_numeros(int vetor[], int n1);
 
 int main() {
     int vetor[100];
-    int n;
+    int n1;
 
     printf("Digite a quantidade de numeros: ");
-    scanf("%d", &n);
+    scanf("%d", &n1);
     printf("Digite os numeros: ");
     
-    for (int i = 0; i < n; i++) {
+    for (int i = 0; i < n1; i++) {
         scanf("%d", &vetor[i]);
     }
-    Ordem_dos_numeros(vetor, n);
+    Ordem_dos_numeros(vetor, n1);
     printf("Numeros ordenados: ");
-    for (int i = 0; i < n; i++) {
+    for (int i = 0; i < n1; i++) {
         printf("%d ", vetor[i]);
     }
     return 0;
 }
 
-void Ordem_dos_numeros(int vetor[], int n) {
+void Ordem_dos_numeros(int vetor[], int n1) {
     int troca;
 
-    for (int i = 0; i < n; i++) {
-        for (int j = i + 1; j < n; j++) {
+    for (int i = 0; i < n1; i++) {
+        for (int j = i + 1; j < n1; j++) {
             if (vetor[i] > vetor[j]) {
                 troca = vetor[i];
                 vetor[i] = vetor[j];
